90f1851b5b4864be5f0f80b6af7654dd421f7db0
Ivan Che <al_chechot@ukr.net>
2019-11-20 02:58:38 +0200

    1st fuck

---
+++ mywindow.h
@@ -0,0 +1,17 @@
+#ifndef MYWINDOW_H
+#define MYWINDOW_H
+
+#include <QWidget>
+
+class MyWindow : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit MyWindow(QWidget *parent = nullptr);
+
+signals:
+
+public slots:
+};
+
+#endif // MYWINDOW_H
